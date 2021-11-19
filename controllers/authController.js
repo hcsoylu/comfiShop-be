bb16d6d3165a95b621e062ec18cb534ccf9e8b33
hüseyin can soylu <hsyncn17@gmail.com>
2021-11-19 12:39:02 +0300

    user route structure

--- controllers/authController.js
+++ controllers/authController.js
@@ -51,7 +51,12 @@ const login = async (req, res) => {
 };
 
 const logout = async (req, res) => {
-  res.send("logout user");
+  res.cookie("token", "logout", {
+    httpOnly: true,
+    expires: new Date(Date.now()),
+  });
+
+  res.status(StatusCodes.OK).send({ msg: "logged out" });
 };
 
 module.exports = {
