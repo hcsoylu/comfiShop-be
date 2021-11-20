8fbd1f925da2c8cc56c46b919a19b9410ec3c887
hüseyin can soylu <hsyncn17@gmail.com>
2021-11-20 15:44:03 +0300

    create review controller

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,5 +1,34 @@
+const Review = require("../models/Review");
+const Product = require("../models/Product");
+
+const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors/");
+const { checkPermissions } = require("../utils");
+
 const createReview = async (req, res) => {
-  res.send("create review");
+  const { product: productId } = req.body;
+
+  const isValidProduct = await Product.findOne({ _id: productId });
+
+  if (!isValidProduct) {
+    throw new CustomError.NotFoundError("no product with this id");
+  }
+
+  const alreadySubmitted = await Review.findOne({
+    product: productId,
+    user: req.user.userId,
+  });
+
+  if (alreadySubmitted) {
+    throw new CustomError.BadRequestError(
+      "You already submitted review for this product"
+    );
+  }
+
+  req.body.user = req.user.userId;
+
+  const review = await Review.create(req.body);
+  res.status(StatusCodes.CREATED).json({ review });
 };
 
 const getAllReviews = async (req, res) => {
